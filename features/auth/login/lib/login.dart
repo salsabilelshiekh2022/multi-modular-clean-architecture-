fbaecbef02a60319ae9e3dd6b16709b9ed2a2919
salsabil <salsabilelshiekh6@gmail.com>
2026-09-18 23:54:04 +0300

    Add session management and app name retrieval to login and profile features

--- features/auth/login/lib/login.dart
+++ features/auth/login/lib/login.dart
@@ -1,5 +1,8 @@
+import 'package:data_source/provider/session_provider_impl.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+  var session = DevSessionProviderImpl();
 }
--- features/settings/profile/lib/profile.dart
+++ features/settings/profile/lib/profile.dart
@@ -1,5 +1,8 @@
+import 'package:multi_modular_clean_arch/main_app_provider.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+  var name = getAppName();
 }
